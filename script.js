81f99bcc212ae51ba05ec170583449caab59ced4
Diraj G <diraj1225@gmail.com>
2026-01-10 11:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.getElementById("bestScore").innerText = bestScore;
 const SCALE = isMobile ? 1.1 : 1.3;
 
 /* GROUND */
-const groundY = canvas.height - (isMobile ? 90 : 110);
+const groundY = canvas.height - (isMobile ? 90 : 80);
 
 /* PLAYER */
 let player;
